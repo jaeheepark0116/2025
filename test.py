ed9a3c4af23f3028e638e80ee7bdd70f73bccfbe
jaeheepark0116 <jaeheepark0116@gmail.com>
2025-08-28 08:58:51 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,53 +1,105 @@
 import streamlit as st
 import random
 
+st.set_page_config(page_title="만화 추천기", page_icon="📚", layout="wide")
+
 # --- 만화 장르별 추천 데이터 ---
 manga_data = {
     "액션": [
-        {"title": "나루토", "desc": "닌자의 세계에서 펼쳐지는 성장과 우정의 이야기", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/9/94/NarutoCoverTankobon1.jpg"},
-        {"title": "원피스", "desc": "해적왕을 꿈꾸는 루피와 동료들의 대모험", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/2/2c/OnePieceVol61Cover.jpg"},
-        {"title": "진격의 거인", "desc": "거인과 인류의 생존 전쟁", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/7/70/Attack_on_Titan_cover.jpg"}
+        {"title": "나루토", "desc": "닌자의 세계에서 펼쳐지는 성장과 우정 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/naruto.jpg"},
+        {"title": "원피스", "desc": "해적왕을 꿈꾸는 루피와 동료들의 대모험",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/onepiece.jpg"},
+        {"title": "진격의 거인", "desc": "거인과 인류의 생존 전쟁",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/aot.jpg"},
+        {"title": "블리치", "desc": "사신의 힘을 얻은 소년의 전투와 성장",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/bleach.jpg"},
+        {"title": "헌터x헌터", "desc": "헌터가 되기 위한 소년의 모험",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/hxh.jpg"}
     ],
     "로맨스": [
-        {"title": "너의 이름은", "desc": "운명처럼 얽히는 두 소년소녀의 이야기", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/0/0b/Your_Name_poster.png"},
-        {"title": "오렌지", "desc": "편지를 통해 미래를 바꾸려는 고등학생들의 이야기", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/7/73/Orange_Volume_1.jpg"},
-        {"title": "토라도라!", "desc": "작지만 무서운 소녀와 순진한 소년의 러브코미디", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/5/5b/Toradora%21_light_novel_volume_1_cover.jpg"}
+        {"title": "너의 이름은", "desc": "운명처럼 얽히는 두 소년소녀 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/yourname.jpg"},
+        {"title": "오렌지", "desc": "편지를 통해 미래를 바꾸려는 고등학생들의 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/orange.jpg"},
+        {"title": "토라도라!", "desc": "작지만 무서운 소녀와 순진한 소년의 러브코미디",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/toradora.jpg"},
+        {"title": "피치 걸", "desc": "사랑과 우정을 그린 학원 로맨스",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/peachgirl.jpg"},
+        {"title": "카드캡터 사쿠라", "desc": "마법과 소년 소녀의 성장 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/ccsakura.jpg"}
     ],
     "판타지": [
-        {"title": "풀메탈 알케미스트", "desc": "연금술 형제가 잃어버린 것을 되찾기 위한 여정", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/2/2a/Fullmetal_vol_1.jpg"},
-        {"title": "블리치", "desc": "사신의 힘을 얻은 소년의 전투와 성장", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/7/72/Bleach_cover_01.jpg"},
-        {"title": "Re:제로부터 시작하는 이세계 생활", "desc": "죽음을 반복하며 운명을 개척하는 소년의 이야기", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/8/8c/Re_Zero_light_novel_volume_1.jpg"}
+        {"title": "풀메탈 알케미스트", "desc": "형제가 잃어버린 것을 되찾는 여정",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/fma.jpg"},
+        {"title": "Re:제로", "desc": "죽음을 반복하며 운명을 개척하는 소년 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/rezero.jpg"},
+        {"title": "소드 아트 온라인", "desc": "가상현실 세계에서의 모험과 사랑",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/sao.jpg"},
+        {"title": "블랙 클로버", "desc": "마법 왕이 되기 위한 소년의 성장기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/blackclover.jpg"},
+        {"title": "페어리 테일", "desc": "길드 동료들과의 모험과 우정",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/fairytail.jpg"}
     ],
     "스포츠": [
-        {"title": "슬램덩크", "desc": "농구를 통해 성장하는 청춘들의 이야기", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/0/04/Slam_Dunk_volume_1_cover.jpg"},
-        {"title": "하이큐!!", "desc": "배구를 향한 열정과 팀워크의 이야기", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/8/86/Haikyu_volume_1.jpg"},
-        {"title": "아이실드 21", "desc": "미식축구를 통해 성장하는 소년의 도전", 
-         "img": "https://upload.wikimedia.org/wikipedia/en/3/37/Eyeshield21_vol01_Cover.jpg"}
+        {"title": "슬램덩크", "desc": "농구를 통해 성장하는 청춘 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/slamdunk.jpg"},
+        {"title": "하이큐!!", "desc": "배구를 향한 열정과 팀워크 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/haikyu.jpg"},
+        {"title": "아이실드 21", "desc": "미식축구를 통해 성장하는 소년 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/eyeshield21.jpg"},
+        {"title": "킹덤", "desc": "중국 전국시대를 배경으로 한 전쟁 만화",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/kingdom.jpg"},
+        {"title": "베이비 스텝", "desc": "테니스를 통해 성장하는 청춘 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/babysteps.jpg"}
+    ],
+    "스릴러": [
+        {"title": "데스노트", "desc": "죽음의 노트를 손에 넣은 소년의 심리 게임",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/deathnote.jpg"},
+        {"title": "몬스터", "desc": "연쇄살인 사건의 진실을 쫓는 의사 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/monster.jpg"},
+        {"title": "도쿄구울", "desc": "인간과 구울 사이의 갈등과 성장",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/tokyoghoul.jpg"}
+    ],
+    "코미디": [
+        {"title": "짱구는 못말려", "desc": "유쾌한 일상과 장난꾸러기 소년 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/crayonshinchan.jpg"},
+        {"title": "은혼", "desc": "사무라이와 코믹 사건이 함께하는 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/gintama.jpg"},
+        {"title": "니세코이", "desc": "사랑과 코미디가 가득한 학원 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/nisekoi.jpg"}
+    ],
+    "공포": [
+        {"title": "링", "desc": "저주받은 비디오 테이프와 공포의 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/ring.jpg"},
+        {"title": "도쿄 이블", "desc": "도쿄를 배경으로 한 스릴러 공포",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/tokyoevil.jpg"}
+    ],
+    "역사": [
+        {"title": "킹덤", "desc": "중국 전국시대 전쟁 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/kingdom.jpg"},
+        {"title": "베르사유의 장미", "desc": "프랑스 혁명을 배경으로 한 역사 로맨스",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/versailles.jpg"}
+    ],
+    "SF": [
+        {"title": "플루토", "desc": "아톰 시리즈를 기반으로 한 SF 추리",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/pluto.jpg"},
+        {"title": "은하철도 999", "desc": "우주 여행과 인간 이야기",
+         "img": "https://raw.githubusercontent.com/nyan-png/manga-images/main/galaxyexpress.jpg"}
     ]
 }
 
-# --- Streamlit 앱 ---
-st.title("📚 만화책 추천기")
+# --- Streamlit 앱 UI ---
+st.title("📚 만화책 추천기 (장르별)")
 
-# 사이드바에서 장르 선택
-genre = st.sidebar.radio("장르를 선택하세요", list(manga_data.keys()))
+# 장르 선택
+genre = st.sidebar.selectbox("장르 선택", list(manga_data.keys()))
 
-# 버튼 누르면 추천
+# 추천 버튼
 if st.button("추천 받기"):
     choice = random.choice(manga_data[genre])
     st.subheader(f"✅ 추천 만화: {choice['title']}")
     st.image(choice["img"], caption=choice["title"], use_column_width=True)
     st.write(choice["desc"])
 else:
-    st.info("👉 사이드바에서 장르를 고르고 '추천 받기' 버튼을 눌러보세요!")
+    st.info("👉 장르를 선택하고 '추천 받기' 버튼을 눌러주세요!")
